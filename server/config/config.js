0b44ccaa0b27e79e09718e1e22f14b25c064fe20
Ambre Gorias <ambre.gorias@gmail.com>
2022-05-10 22:34:41 -1000

    backend deployment configuration

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,27 @@
 const dotenv = require('dotenv').config();
 
 module.exports = {
+  production: {
+    username: process.env.DB_USERNAME_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: process.env.DB_NAME_PROD,
+    host: process.env.HOST_PROD,
+    dialect: process.env.DIALECT,
+  },
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.HOST,
-    dialect: process.env.DIALECT,
+    dialect: 'mysql',
   },
 };
+// module.exports = {
+//   development: {
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     host: process.env.HOST,
+//     dialect: 'mysql',
+//   },
+// };
